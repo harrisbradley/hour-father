5138ef94bf725ffdce5720411fec41a800ae8e5f
Sarge <bradleyharris415@gmail.com>
2025-04-29 23:55:41 +0000

    starting over, take 2

---
+++ hour-father/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+// src/App.js
+function App() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Hour Father 🙏</h1>
+      <p>The app is working!</p>
+    </div>
+  );
+}
+
+export default App;
